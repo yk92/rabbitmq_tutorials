e2238edc0d38791fb88a0e04c55f1b31d1a6ac65
Yuval <yk92@njit.edu>
2016-09-16 23:36:03 -0400

    Created emit_logs and receive_logs to test exchanges

--- new_task.py
+++ new_task.py
@@ -7,11 +7,11 @@ connection = pika.BlockingConnection(pika.ConnectionParameters( host='localhost'
 
 channel = connection.channel()
 
-channel.queue_declare(queue='task_queue')
+channel.queue_declare(queue='durable_queue', durable=True)
 
 message = ' '.join(sys.argv[1:]) or "Hello World!"
 
-channel.basic_publish(exchange='', routing_key='task_queue', body=message, properties=pika.BasicProperties(delivery_mode = 2, #make message persistent
+channel.basic_publish(exchange='', routing_key='durable_queue', body=message, properties=pika.BasicProperties(delivery_mode = 2, #make message persistent
                  ))
 
 print( " [x] Sent %r" % message)
