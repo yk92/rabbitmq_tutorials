18e29f0c48e5011aa288b080f4ca0260a91dea1e
Yuval <yk92@njit.edu>
2016-09-16 22:28:06 -0400

    Added prefetch count to queue

--- worker.py
+++ worker.py
@@ -6,7 +6,8 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost')
 
 channel = connection.channel()
 
-channel.queue_declare(queue='task_queue')
+channel.queue_declare(queue='durable_queue', durable=True)
+print(' [x] Waiting for messages. To exit press CTRL+C')
 
 def callback(ch, method, properties, body):
 	print(" [x] Received %r" % body)
@@ -14,8 +15,7 @@ def callback(ch, method, properties, body):
 	print(" [x] Done")
 	ch.basic_ack(delivery_tag = method.delivery_tag)
 
-channel.basic_consume(callback, queue='task_queue')
-
-print(' [x] Waiting for messages. To exit press CTRL+C')
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(callback, queue='durable_queue')
 
 channel.start_consuming()
